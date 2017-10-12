7a25fe57a9a8b4cacc77f6dd9520b86bd54d7d62
EAS7510 <michaeleaster@northwesternmutual.com>
2017-10-12 12:06:39 -0500

    Update.

--- project.clj
+++ project.clj
@@ -1,5 +1,12 @@
-(defproject org.clojars.mike706574/cities "0.0.1-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
-                 [org.clojure/test.check "0.9.0"]]
+(defproject fun.mike/cities "0.0.1-SNAPSHOT"
+  :description "Lost Cities in Clojure."
+  :url "https://github.com/mike706574/cities"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+                 [org.clojure/test.check "0.10.0-alpha2"]]
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[org.clojure/tools.namespace "0.2.11"]]}})
+                   :target-path "target/dev"
+                   :dependencies [[org.clojure/tools.namespace "0.2.11"]]}}
+  :repl-options {:init-ns user})
